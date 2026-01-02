f6ba3e6421d3d1d91e876b206308bbab51794171
Vikalp Tomar <vikalptomar100@gmail.com>
2026-01-03 01:52:22 +0530

    icons added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ export const metadata = {
   title: "AKT LAW FIRM",
   description: "A professional law firm dedicated to providing exceptional legal services.",
   keywords: "law firm, legal services, attorney, lawyer",
+  icons: {
+    icon: "/logo1.webp",
+  },
 };
 
 export default function RootLayout({ children }) {
